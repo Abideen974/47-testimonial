2a3f16a98dee64eca146dde79d1dc860c3e5f9c7
Abideen <Abideenali926@gmail.com>
2022-04-21 02:26:14 -0700

    testimonial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Testmonial from './components/Testmonial'
+
+function App() {
+  return (
+    <div className="App">
+      <Testmonial />
+    </div>
+  );
+}
+
+export default App;
+
+// netlify url https://testimonial-47.netlify.app/
